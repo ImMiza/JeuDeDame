a3ce268276b2239f6163a2552a2504dd9e3e5c79
allan <allan150199@outlook.fr>
2018-09-24 19:01:29 +0200

    alpha03
    Conception de la map (background)
    Ligne et case de couleur

--- JeuDeDame/src/application/Main.java
+++ JeuDeDame/src/application/Main.java
@@ -1,25 +1,45 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
+import me.allan.ressources.DameMap;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
-
 public class Main extends Application {
+	
+	
+	private static Stage window;
+	private static BorderPane root;
+	private static Scene scene;
+	private static DameMap map;
+	
+	
+	public void initialize() {
+		map = new DameMap(window);
+		root.getChildren().add(map);
+	}
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
+			
+			window = primaryStage;
+			root = new BorderPane();
+			scene = new Scene(root, 1000, 700);
+			
+			window.setTitle("Jeu De Dame By MizakiGaming MF");
+			window.setScene(scene);
+			window.show();
+			
+			initialize();
+			
+			
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
